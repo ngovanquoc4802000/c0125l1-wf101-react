fbbefb04324bc3ebce00e4f3a71a30e04709d8ef
ngovanquoc <ngovanquoc480@gmail.com>
2025-06-06 19:20:10 +0700

    push

--- react/lesson-basic-react/src/react-function/lesson_four/lesson_basic_6.tsx
+++
@@ -1,9 +0,0 @@
-import LessonBasicFive from "./lesson_basic_5";
-
-export default function LessonBasicSix() {
-  return (
-    <>
-    <LessonBasicFive props="This is the data from the parent component" />
-    </>
-  )
-}
